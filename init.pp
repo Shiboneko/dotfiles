3b0b70d8a53e10b2277fd32beb544f3bb39e2685
user <user@Sanchez.fritz.box>
2016-11-12 19:13:55 +0100

    Added install script and puppet manifest

---
+++ init.pp
@@ -0,0 +1,5 @@
+
+
+	Package { ensure => 'installed' }
+	package { 'emacs': }
+	package { 'eclipse': }
--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+	    exit
+fi
+pacman -S puppet
+puppet apply init.pp
+
 mkdir ~/.emacs.d/
-mkdir ~/.config/i#!/bin/bash
+mkdir ~/.config/
 
